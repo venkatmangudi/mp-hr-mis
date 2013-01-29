7041d1cfcd0a78207f5e090969f90517dc3468db
SkyKOG <akashdevaraju@gmail.com>
2013-01-29 15:03:39 +0530

    Validations are working

--- railstest-testing/beauty/app/models/state.rb
+++ railstest-testing/beauty/app/models/state.rb
@@ -1,4 +1,5 @@
 class State < ActiveRecord::Base
   attr_accessible :state_name
   belongs_to :location
+  validates_uniqueness_of(:state_name, :message => "There is already a State with that name")
 end
