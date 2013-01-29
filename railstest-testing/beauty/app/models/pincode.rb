72383e8ef9e8f6ebe7fbe0acb86b799a06088054
SkyKOG <akashdevaraju@gmail.com>
2013-01-29 15:40:11 +0530

    Removed Destroy,Added strict validation

--- railstest-testing/beauty/app/models/pincode.rb
+++ railstest-testing/beauty/app/models/pincode.rb
@@ -1,5 +1,8 @@
 class Pincode < ActiveRecord::Base
   attr_accessible :pincode
   belongs_to :location
+  
+  validates_presence_of(:pincode)
+  validates_numericality_of(:pincode, :only_integer => true ,:message => "Has to be Numerical")
   validates_uniqueness_of(:pincode, :message => "There is already a Pincode with that name")
 end
