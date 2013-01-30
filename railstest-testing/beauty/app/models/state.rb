049479afcfb631db872550ddac6afba622ae3a28
SkyKOG <akashdevaraju@gmail.com>
2013-01-30 18:17:42 +0530

    Associations SUCCESS

--- railstest-testing/beauty/app/models/state.rb
+++ railstest-testing/beauty/app/models/state.rb
@@ -1,7 +1,6 @@
 class State < ActiveRecord::Base
   attr_accessible :state_name
   has_many:locations
-  has_many:dropthings
   validates_uniqueness_of(:state_name)
   validates_presence_of(:state_name)
 end
