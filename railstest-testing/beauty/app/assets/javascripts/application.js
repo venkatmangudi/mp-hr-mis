51894a651ea09c014e0fd5a1d835f8a535451222
SkyKOG <akashdevaraju@gmail.com>
2013-02-01 11:18:38 +0530

    X-Editable test

--- railstest-testing/beauty/app/assets/javascripts/application.js
+++ railstest-testing/beauty/app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require bootstrap-editable
+//= require bootstrap-editable-rails
 //= require_tree .
 //= require dataTables/jquery.dataTables
 //= require dataTables/jquery.dataTables.bootstrap
